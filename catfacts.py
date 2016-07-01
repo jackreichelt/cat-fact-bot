878ff511d92838d8e25e14c2a64a66b64f57a888
jackreichelt <jackreichelt@gmail.com>
2016-07-01 10:03:44 +1000

    V1

---
+++ catfacts.py
@@ -0,0 +1,60 @@
+from random import choice
+
+class CatFacts(object):
+
+  facts = []
+  subscribers = {} # Format: {user_id: {fact_key: fact_count}}
+  fact_count = 0
+
+  def __init__(self):
+    self.facts = open('facts.txt').readlines()
+
+    self.fact_count = len(self.facts)
+
+    subs = open('subscribers.txt')
+    for line in subs:
+      chunks = line.split(',')
+
+      received_facts = {}
+      for fact in chunks[1:]:
+        fact = fact.split(':')
+        received_facts[fact[0]] = int(fact[1])
+
+      self.subscribers[chunks[0]] = received_facts
+
+  def add_subscriber(self, user_id):
+    if user_id in self.subscribers: return
+
+    self.subscribers[user_id] = {}
+
+  def remove_subscriber(self, user_id):
+    if user_id not in self.subscribers: return
+    self.subscribers.pop(user_id)
+
+  def get_fact(self, subscriber = None):
+    fact = choice(range(self.fact_count))
+
+    i = 0
+    while subscriber != None and \
+          fact in self.subscribers[subscriber] and \
+          self.subscribers[subscriber][fact] == max(self.subscribers[subscriber].values()) and \
+          i < 10:
+      i += 1
+      fact = choice(range(self.fact_count))
+
+    if subscriber != None:
+      if fact not in self.subscribers[subscriber]:
+        self.subscribers[subscriber][fact] = 1
+      else:
+        self.subscribers[subscriber][fact] += 1
+
+    return self.facts[fact]
+
+  def write_subscribers(self):
+    subs = open('subscribers.txt', 'w')
+    for user, facts in subs.iteritems():
+      subs.write(user)
+      for fact, count in facts.iteritems():
+        subs.write(',' + fact + ':')
+      subs.write('\n')
+    subs.close
---
+++ tutorbot.py
@@ -0,0 +1,76 @@
+from time import sleep
+from slackclient import SlackClient
+import os
+from datetime import datetime
+from catfacts import *
+from random import choice
+
+NAME = 'cat_facts'
+
+def get_icon_emoji():
+  return choice([
+    ':smiley_cat:',
+    ':smile_cat:',
+    ':joy_cat:',
+    ':heart_eyes_cat:',
+    ':smirk_cat:',
+    ':kissing_cat:',
+    ':scream_cat:',
+    ':crying_cat_face:',
+    ':pouting_cat:',
+    ':cat:',
+    ':cat2:'
+  ])
+
+posted = False
+
+cf = CatFacts()
+channels = {} #Mapping from channel_id to user_id
+
+usage = """
+Welcome to Cat Facts!
+To subscribe to your daily cat fact, tag me and say "subscribe".
+To unsubscribe from this service, tag me and say "unsubscribe".
+To get a fact right now, tag me and say "fact"!
+
+Anything else and I'll show you this message to help you out!
+
+If you have any facts you want to add, comments, complaints, or bug reports, message Jack Reichelt.
+"""
+
+token = os.environ.get('TOKEN', None) # found at https://api.slack.com/web#authentication
+sc = SlackClient(token)
+if sc.rtm_connect() == True:
+  print('Connected.')
+
+  sc.api_call("im.list")
+
+  while True:
+    response = sc.rtm_read()
+    for part in response:
+      if 'ims' in part:
+        channels = part['ims']
+      if part['type'] == 'message':
+        if '<@U1MKHKV8U>' in part['text']:
+          if 'unsubscribe' in part['text'].lower():
+            cf.remove_subscriber(part['user'])
+            channels.pop(part['channel'])
+          elif 'subscribe' in part['text'].lower():
+            cf.add_subscriber(part['user'])
+            channels[part['channel']] = part['user']
+          elif 'fact' in part['text'].lower():
+            sc.api_call("chat.postMessage", channel=part['channel'], text=cf.get_fact(part['user']), username=NAME, icon_emoji=get_icon_emoji())
+          else:
+            sc.api_call("chat.postMessage", channel=part['channel'], text=usage, username=NAME, icon_emoji=get_icon_emoji())
+          #sc.api_call("chat.postMessage", channel=part['channel'], text=message, username='tutorbot', icon_emoji=':mortar_board:')
+
+    if 14 < datetime.utcnow().time().hour() < 15: #midnight to 1am
+      posted = False
+    if 05 < datetime.utcnow().time().hour() < 06 and posted == False: #3pm to 4pm
+      posted = True
+      for channel, user in channels.iteritems():
+        sc.api_call("chat.postMessage", channel=channel, text=cf.get_fact(user), username=NAME, icon_emoji=get_icon_emoji())
+
+    sleep(1)
+else:
+  print('Connection Failed, invalid token?')
